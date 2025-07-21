c54c936acb1105f119b1857ac42639a2a6e3ee30
Азисбек <azisbek@MacBook-Air-Azisbek.local>
2025-07-21 17:11:02 +0600

    fix:test husky

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -7,12 +7,8 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
 }
 
-export const Button = ({
-  className,
-  children,
-  ...resProps
-}: ButtonProps) => (
-  <button type='button' className={clsx(s.button, className)} {...resProps}>
+export const Button = ({ className, children, ...resProps }: ButtonProps) => (
+  <button type="button" className={clsx(s.button, className)} {...resProps}>
     {children}
   </button>
 );
