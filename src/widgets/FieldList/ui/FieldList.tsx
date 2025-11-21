d677e17ea9a52146fa5e736530c84265f0f07bef
DSanchos <norms777666@gmail.com>
2025-11-21 13:54:47 +0600

    fix:fix error with index in field list 2

--- src/widgets/FieldList/ui/FieldList.tsx
+++ src/widgets/FieldList/ui/FieldList.tsx
@@ -4,10 +4,8 @@ import s from "./FieldList.module.scss";
 export function FieldList() {
   return (
     <div className={s.fieldList}>
-      {/* Временно пока данные не будут готовы  */}
-      {}
-      {[1, 2, 3].map((_, i) => (
-        <FieldCard key={i} />
+      {[1, 2, 3].map((n) => (
+        <FieldCard key={n} />
       ))}
     </div>
   );
