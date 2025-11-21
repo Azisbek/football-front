5a359b15fc1eb1751b6ea4f5335f45d019b770b7
DSanchos <norms777666@gmail.com>
2025-11-21 14:15:53 +0600

    fix:field list deleted maping cards and added three card

--- src/widgets/FieldList/ui/FieldList.tsx
+++ src/widgets/FieldList/ui/FieldList.tsx
@@ -4,9 +4,9 @@ import s from "./FieldList.module.scss";
 export function FieldList() {
   return (
     <div className={s.fieldList}>
-      {[1, 2, 3].map((n) => (
-        <FieldCard key={n} />
-      ))}
+      <FieldCard />
+      <FieldCard />
+      <FieldCard />
     </div>
   );
 }
